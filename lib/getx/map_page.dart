dd5ea7bc6fb5629dc12fe411013958362235dd66
chooyan-eng <chooyan.engineer@gmail.com>
2023-08-20 17:16:12 +0900

    use shared widget to GetX version

--- lib/getx/map_page.dart
+++ lib/getx/map_page.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
 import 'package:get/get.dart';
-import 'package:latlong2/latlong.dart';
 import 'package:state_management_geoapp/getx/point_list_page.dart';
 import 'package:state_management_geoapp/getx/state/point_list_controller.dart';
 import 'package:state_management_geoapp/shared/model/point.dart';
+import 'package:state_management_geoapp/shared/widget/map.dart';
 
 class MapPage extends StatelessWidget {
   const MapPage({super.key});
@@ -26,37 +25,14 @@ class MapPage extends StatelessWidget {
       ),
       body: Obx(
         () {
-          final list = controller.pointList;
-          // TODO(chooyan-eng): can't use shared [MyMap] widget
-          return FlutterMap(
-            options: MapOptions(
-              center: const LatLng(51.509364, -0.128928),
-              zoom: 9.2,
-              onLongPress: (_, position) {
-                controller.add(
-                  Point(comment: '', coordinate: position),
-                );
-              },
-            ),
-            children: [
-              TileLayer(
-                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-              ),
-              MarkerLayer(
-                markers: list
-                    .map(
-                      (point) => Marker(
-                        point: point.coordinate,
-                        builder: (context) => const Icon(
-                          Icons.maps_ugc,
-                          size: 24,
-                          color: Colors.blue,
-                        ),
-                      ),
-                    )
-                    .toList(),
-              ),
-            ],
+          final list = controller.pointList.toList();
+          return MyMap(
+            pointList: list,
+            onLongPress: (position) {
+              controller.add(
+                Point(comment: '', coordinate: position),
+              );
+            },
           );
         },
       ),
--- lib/getx/point_list_page.dart
+++ lib/getx/point_list_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:state_management_geoapp/getx/state/point_list_controller.dart';
+import 'package:state_management_geoapp/shared/widget/point_list.dart';
 
 class PointListPage extends StatelessWidget {
   PointListPage({super.key});
@@ -16,29 +17,11 @@ class PointListPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('Point List Page')),
       body: Obx(() {
-        // TODO(chooyan-eng): can't use shared [PointList] widget
-        return ListView.builder(
-            itemCount: controller.pointList.length,
-            itemBuilder: (context, index) {
-              final point = controller.pointList[index];
-              return InkWell(
-                onTap: () {},
-                child: Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: Column(
-                    children: [
-                      Text(
-                        'lat: ${point.coordinate.latitude} / lng: ${point.coordinate.longitude}',
-                      ),
-                      if (point.comment.isNotEmpty) ...[
-                        const SizedBox(height: 8),
-                        Text(point.comment),
-                      ]
-                    ],
-                  ),
-                ),
-              );
-            });
+        final list = controller.pointList.toList();
+        return PointList(
+          data: list,
+          onTap: (value) {},
+        );
       }),
     );
   }
